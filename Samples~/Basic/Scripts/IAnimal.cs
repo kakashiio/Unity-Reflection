256eacb17b49c7efc0d7e681562fb12dc810b8ff
kakashi <junhong.cai@kunlun-inc.com>
2022-05-02 00:39:50 +0800

    Modify the namespace

---
+++ Samples~/Basic/Scripts/IAnimal.cs
@@ -0,0 +1,19 @@
+namespace IO.Unity3D.Source.Reflection.Samples.Basic
+{
+    //******************************************
+    //  
+    //
+    // @Author: Kakashi
+    // @Email: john.cha@qq.com
+    // @Date: 2022-04-25 22:50
+    //******************************************
+    public interface IAnimal
+    {
+        void Run();
+
+        [RefMethod]
+        void Sleep();
+
+        void Eat();
+    }
+}
